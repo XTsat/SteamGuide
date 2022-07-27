87ecfb82ecf27991ca962abfcc1437fc942ae51f
XTsat <44708609+XTsat@users.noreply.github.com>
2022-07-28 02:32:18 +0800

    navbar 文本化

--- docs/.vuepress/configs/navbar/zh.ts
+++ docs/.vuepress/configs/navbar/zh.ts
@@ -7,7 +7,7 @@ export const zh: NavbarConfig = [
   },
   {
     text: 'Steam 指南',
-    link: '/document/',
+    link: '/document/Profile/展柜展示.md',
   },
   {
     text: '官方',
@@ -28,58 +28,6 @@ export const zh: NavbarConfig = [
   },
   {
     text: '关于我',
-    children: [
-      {
-        text: '指南',
-        children: [
-          {
-            text: 'Steam',
-            link: 'https://xtsat.github.io/SteamGuide/'
-          },
-          {
-            text: '瓦特工具箱',
-            link: 'https://xtsat.github.io/SteamTools-Guide/'
-          },
-          {
-            text: '浏览器',
-            link: 'https://xtsat.github.io/Browser-Guide/'
-          },
-          {
-            text: 'Windows',
-            link: 'https://xtsat.github.io/Windows-Guide/'
-          },
-        ]
-      },
-      {
-        text: '脚本',
-        children: [
-          {
-            text: '秒传链接提取',
-            link: 'https://xtsat.github.io/rapid-upload-userscript-doc/'
-          },
-        ]
-      },
-      {
-        text: '其他平台',
-        children: [
-          {
-            text: 'BiliBili 📺',
-            link: 'https://space.bilibili.com/53283118'
-          },
-          {
-            text: 'Steam 🎮',
-            link: 'https://steamcommunity.com/id/XTxiaotong'
-          },
-          {
-            text: 'Playlist 🎵',
-            link: 'https://music.163.com/#/user/home?id=283588276'
-          },
-          {
-            text: 'GitHub',
-            link: 'https://github.com/XTsat'
-          },
-        ]
-      },
-    ]
+    link: '/关于我.md',
   },
 ]
